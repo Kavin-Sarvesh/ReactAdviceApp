fd548aa5da05f6062dc39737019c0fcdeeafc57b
Sarvesh <ksarvesh.salem@gmail.com>
2024-02-05 20:41:46 +0530

    qrgenerate

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
-import './index.css'
-import { UserCard } from './UserCard'
+// import './index.css'
+import './QrCode.css'
+import { QrCode } from './QrCode'
+// import { UserCard } from './UserCard'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <UserCard />
+    {/* <App />
+    <UserCard /> */}
+    <QrCode />
   </React.StrictMode>
 )
